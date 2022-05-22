23f9ee73591cb27d3290d5bef010fa607cbedad4
Bartosz Sietejko <barto32224@wp.pl>
2022-05-22 21:34:47 +0200

    Finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Dot from "./Dot";
+import React, {useState} from 'react';
 
 function App() {
+
+    const [isActive, setIsActive] = useState(null);
+
     return (
         <div className="dots-wrapper">
             {new Array(20).fill('#000000').map((_, i) => (
-                <Dot key={i}/>
+                <Dot key={i} value={i} isActive={isActive} setIsActive={setIsActive}/>
             ))}
         </div>
     );
--- src/Dot.js
+++ src/Dot.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import './Dot.css';
+import React, { useState } from "react";
+import "./Dot.css";
 
-const Dot = () => {
-    // TODO: put your code here
-    return (
-        <div className="dot"/>
-    );
+function randomColor() {
+  return Math.floor(Math.random() * 1000000);
+}
+
+const Dot = (props) => {
+  const [color, setColor] = useState(null);
+
+  const handleClick = () => {
+    props.setIsActive(props.value);
+    setColor("#" + randomColor());
+  };
+
+  return (
+    <div
+      className="dot"
+      onClick={handleClick}
+      style={{
+        backgroundColor: props.isActive === props.value && color,
+      }}
+    >
+      {props.isActive === props.value && props.value + 1}
+    </div>
+  );
 };
 
 export default Dot;
